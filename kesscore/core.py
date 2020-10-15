6ee4d4fac4dc9c124643f09668d4d4ce56e91fd4
Ido Kessler <kessido@gmail.com>
2020-10-15 16:57:05 +0300

    test

--- kesscore/core.py
+++ kesscore/core.py
@@ -5,4 +5,7 @@ __all__ = ['upfl', 'upfls', 'say_hello']
 # Cell
 def upfl(w:str):'Upper First Letter'; return w[:1].upper() + w[1:]
 def upfls(ws:str):'Upper First Letters'; return ' '.join(map(upfl,ws.split(' ')))
-def say_hello(to:str):"Say hello to somebody"; return f'Hello {upfls(to)}!'
+
+# Cell
+def say_hello(to:str):
+    "Say hello to somebody"; return f'Hello {upfls(to)}!'
