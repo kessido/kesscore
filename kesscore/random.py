3670e5db5a9a82be6fd36247934f0f4aa6bdd1fd
Ido Kessler <kessido@gmail.com>
2020-10-21 01:26:35 +0300

    added perm to random and edited images with typedisptach

--- kesscore/random.py
+++ kesscore/random.py
@@ -4,40 +4,62 @@ __all__ = ['RandomState']
 
 # Cell
 from .imports import *
+from .functional import *
 
 # Cell
-def _rangechoice(nprs,len,size,replace,cum_weights,p):
-    size,pyrs = ifnone(size,1),random.Random(nprs.random())
+def _rangechoice(len,size=None,nprs=None,replace=None,cum_weights=None,p=None):
+    k,pyrs = ifnone(size,1),random.Random(nprs.random())
     if replace:
         assert p is None,'Should use cum_weights'
-        if cum_weights is None: return nprs.randint(len,size=size)
-        else:                 return pyrs.choices(range(len),k=size,cum_weights=cum_weights)
+        if cum_weights is None: idx = nprs.randint(len,size=k)
+        else:                   idx = pyrs.choices(range(len),k=k,cum_weights=cum_weights)
                                      # ^ here can also provide weight=p but performencewise not.
     else:
         assert cum_weights is None,'Should use p'
-        if p is None: return pyrs.sample(range(len),size)
-        else:         return nprs.choice(np.arange(len),size=size,replace=False,p=p)
+        if p is None: idx = pyrs.sample(range(len),k)
+        else:         idx = nprs.choice(np.arange(len),size=k,replace=False,p=p)
+    return idx[0] if size == None else idx
 
-def _choice(nprs,a,size=None,replace=True,cum_weights=None,p=None):
-    if isinstance(a,(dict, range)): a=list(a)
-    if isinstance(a,(tuple,list )): a=L(a)
-    assert isinstance(a, (L,np.ndarray,torch.Tensor))
-    idx = _rangechoice(nprs, len=len(a),size=size,replace=replace,cum_weights=cum_weights,p=p)
-    if size == None: idx=idx[0] # instead or returning something like a[[1]] will rerturn a[1]
-    return a[idx]
+@delegates(_rangechoice, but=['len'])
+def _customrangechoice(rng,**kwargs):
+    idx = _rangechoice(len(rng),**kwargs)
+    if not isinstance(idx, Iterable): return rng[idx]
+    else: return np.asarray(idx) * rng.step + rng.start
+
+@typedispatch
+def _choicedsp(a,**kwargs):                  raise ValueError(f'unknown type {type(a)}')
+@typedispatch
+def _choicedsp(a:typing.Dict,**kwargs):      return _choicedsp(list(a), **kwargs)
+@typedispatch
+def _choicedsp(a:numbers.Integral,**kwargs): return _choicedsp(range(a),**kwargs)
+@typedispatch
+def _choicedsp(a:slice,**kwargs):            return _choicedsp(range(a.start,a.stop,a.step),**kwargs)
+@typedispatch
+def _choicedsp(a:range,**kwargs):            return _customrangechoice(a,**kwargs)
+@typedispatch
+def _choicedsp(a:(np.ndarray,torch.Tensor,L),**kwargs): idx=_rangechoice(len(a),**kwargs); return a[idx]
+@typedispatch
+def _choicedsp(a:(list, tuple),**kwargs):
+    idx = _rangechoice(len(a),**kwargs);
+    return a[idx] if kwargs['size'] is None else lmap(lambda i:a[i], idx)
+
+@delegates(_rangechoice)
+def _choice(a,nprs,size=None,**kwargs): return _choicedsp(a,nprs=nprs,size=size,**kwargs)
 
 # Cell
 @docs
 class RandomState:
     def __init__(self, seed=None): self.rs=np.random.RandomState(seed=seed)
-    def choice(self,a,size=None,cum_weights=None): return _choice(self.rs,a,size=size,replace=True, cum_weights=cum_weights)
-    def sample(self,a,size=None,cum_weights=None): return _choice(self.rs,a,size=size,replace=False,cum_weights=cum_weights)
+    def perm(self,a): return self.rs.permutation(range(a) if isinstance(a,numbers.Integral) else a)
+    def choice(self,a,size=None,cum_weights=None): return _choice(a,self.rs,size=size,replace=True, cum_weights=cum_weights)
+    def sample(self,a,size=None,p=None):           return _choice(a,self.rs,size=size,replace=False,p=p)
     def randn(self,size=None):return self.rs.randn(*size)
     def rand(self,size=None): return self.rs.random(size=size)
     def bool(self,size=None,p=0.5): return self.rand(size) < p
     def int(self,low,high=None,size=None,dtype=int): return self.rs.randint(low=low,high=high,size=size,dtype=dtype)
     _docs = dict(
-        cls_doc = "The class docstring",
+        cls_doc = 'RandomState numpy\pythonic. if seed not given, randomly select one.',
+        perm    = 'Return a random permutation',
         choice  = 'Get sample with replacements',
         sample  = 'Get sample w.o  replacements',
         randn   = 'Return floats in [0, 1)',
